7967c0a7306cdcc1033498a5f0ae62486d85ca02
Robert Ostrowski <robbostrowski@gmail.com>
2020-03-01 19:02:24 +0100

    gitattributes and pacakge clear

--- src/main/java/rozi/Main.java
+++ src/main/java/rozi/Main.java
@@ -1,11 +1,5 @@
 package rozi;
 
-import rozi.gui.GUI;
-import rozi.gui.GuiActionHandler;
-import rozi.gui.GuiState;
-
-import javax.swing.*;
-
 public class Main {
 
     public static void main(String[] args) {
